20264a2b6a723555879bdbf5d3af9a0fc1d8078c
Gabriel Oliveira <gabriel9938@gmail.com>
2021-03-24 19:18:52 -0300

    Finalizando pagina home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,24 @@
 import Head from 'next/head';
+import styles from './home.module.scss'
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Inicio | Ig.News</title>
+        <title>Home | Ig.News</title>
       </Head>
-      <div>
-        Hello World
-      </div>
+
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>News about the <span>React</span> world.</h1>
+          <p>
+            Get access to all the publications <br/>
+            <span>for $9.90 month</span>
+          </p>
+        </section>
+        <img src="/images/avatar.svg" alt="girl codding"/>
+      </main>
     </>
   )
 }
