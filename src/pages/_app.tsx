6fa7a6766bfadf1af6160753bef780cfaaecd82a
Gabriel Oliveira <gabriel9938@gmail.com>
2021-03-24 18:55:50 -0300

    Criando componente Header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,16 @@
 import { AppProps } from 'next/app';
+import React from 'react';
+import { Header } from '../components/Header';
 
 import '../styles/global.scss';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Header />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,6 +8,8 @@ class MyDocument extends Document {
         <Head>
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,500;0,700;1,900&display=swap" rel="stylesheet" />
+
+          <link rel="shortcut icon" href="image/favicon.png" type="image/png"/>
         </Head>
         <body>
           <Main />
