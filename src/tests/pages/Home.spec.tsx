93f42e768e3af6519cb012238764d276346c3e3e
Gabriel Oliveira <gabriel9938@gmail.com>
2021-05-21 17:56:55 -0300

    Testando pagina home

---
+++ src/tests/pages/Home.spec.tsx
@@ -0,0 +1,45 @@
+import { render, screen } from '@testing-library/react';
+import { stripe } from '../../services/stripe'
+import { mocked } from 'ts-jest/utils';
+
+import Home, { getStaticProps } from '../../pages';
+
+jest.mock('next/router')
+jest.mock('next-auth/client', () => {
+  return {
+    useSession() {
+      return [null, false]
+    }
+  }
+})
+jest.mock('../../services/stripe')
+
+describe('Home page', () => {
+  it('renders correctly', () => {
+    render(<Home product={{ priceId: 'fake-price-id', amount: 'R$10,00'}} />)
+
+    expect(screen.getByText('for R$10,00 month')).toBeInTheDocument()
+  });
+
+  it('loads initial data', async () => {
+    const retrieveStripePriceMocked = mocked(stripe.prices.retrieve)
+
+    retrieveStripePriceMocked.mockResolvedValueOnce({
+      id: 'fake-price-id',
+      unit_amount: 1000,
+    } as any )
+
+    const response = await getStaticProps({})
+
+    expect(response).toEqual(
+      expect.objectContaining({
+        props: {
+          product: {
+            priceId: 'fake-price-id',
+            amount: '$10.00'
+          }
+        }
+      })
+    )
+  })
+})
